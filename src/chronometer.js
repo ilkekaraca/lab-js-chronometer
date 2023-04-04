c8951debfd81cad18f8bd02924ee5e7554d087f3
“Ilke <ilkedidem@gmail.com>
2023-04-04 18:10:20 +0200

    Chronometers Changes

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,50 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    //Interval that runs the function once per second
+    this.intervalId = setInterval(() => {
+      // Increment the current time by 1
+      this.currentTime += 1;
+      // If the callback function is passed as an argument, call it
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+      // If the callback function is not passed as an argument, do nothing
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    const valueString = value.toString();
+    if (valueString.length === 1) {
+      return "0" + valueString.slice();
+    }
+    return valueString;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
+    this.currentTime = 0;
     // ... your code goes here
   }
 
   split() {
-    // ... your code goes here
+    const mm = this.computeTwoDigitNumber(this.getMinutes());
+    const ss = this.computeTwoDigitNumber(this.getSeconds());
+    return `${mm}:${ss}`;
   }
 }
